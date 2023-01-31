eb452497df80434a2a86d659f03febb70ccd27c8
liyu <liyu@espressif.com>
2023-01-31 20:25:09 +0800

    添加查询

--- src/http/temperature.rs
+++ src/http/temperature.rs
@@ -1,15 +1,16 @@
 use crate::http::{ApiContext, Result};
 use anyhow::Context;
-use axum::extract::Extension;
+use axum::extract::{Extension, Query};
 use axum::routing::{get, post};
 use axum::{Json, Router};
+use clap::builder::Str;
+use time::{PrimitiveDateTime, Time};
 
 use crate::http::error::{Error, ResultExt};
 
 pub fn router() -> Router {
     Router::new()
-        // .route("/api/temperature", get())
-        // .route("/api/humidity", get(login_user))
+        .route("/api/parameters", get(get_parameters))
         .route("/api/submit", post(submit_date))
 }
 
@@ -24,29 +25,47 @@ struct NewTarget {
     humidity: f64,
 }
 
+#[derive(serde::Deserialize,serde::Serialize)]
+struct DateTarget {
+    data: f64,
+    date: PrimitiveDateTime,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DataQuery {
+    data_type: String,
+    start_date: PrimitiveDateTime,
+    end_date: PrimitiveDateTime
+}
+
+#[axum::debug_handler]
+async fn get_parameters(
+    ctx: Extension<ApiContext>,
+    query: Query<DataQuery>,
+) -> Result<Json<TargetBody<Vec<DateTarget>>>> {
+    let res = sqlx::query_as!(
+            DateTarget,
+        "select temperature as data, created_at as date  from temperature where created_at >= ? and created_at <= ?",
+        query.start_date,
+        query.end_date
+    ).fetch_all(&ctx.db).await?;
+    Ok(Json(TargetBody {
+        data: res
+    }))
+}
+
 
 async fn submit_date(
     ctx: Extension<ApiContext>,
-    Json(req): Json<TargetBody<NewTarget>>,
+    Json(req): Json<NewTarget>,
 ) -> Result<Json<TargetBody<bool>>> {
+    let res = sqlx::query!(
+        "insert into temperature ( temperature, humidity) values (?, ?) ",
+        req.temperature,
+        req.humidity,
+    ).execute(&ctx.db).await?;
 
-    // let user_id = sqlx::query_scalar!(
-    //     // language=PostgreSQL
-    //     r#"insert into "user" (username, email, password_hash) values ($1, $2, $3) returning user_id"#,
-    //     req.user.username,
-    //     req.user.email,
-    //     password_hash
-    // )
-    // .fetch_one(&ctx.db)
-    // .await
-    // .on_constraint("user_username_key", |_| {
-    //     Error::unprocessable_entity([("username", "username taken")])
-    // })
-    // .on_constraint("user_email_key", |_| {
-    //     Error::unprocessable_entity([("email", "email taken")])
-    // })?;
-
-    Ok(Json(TargetBody::<bool>{
+    Ok(Json(TargetBody::<bool> {
         data: true,
     }))
 }
