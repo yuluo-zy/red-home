22f4b73ff367a9c7198b810257f44895f1d20766
yuluo <yuluozy@outlook.com>
2023-02-01 21:53:34 +0800

    add url

--- src/crawlers/electricity.rs
+++ src/crawlers/electricity.rs
@@ -1,4 +1,4 @@
-
+const url = "http://www.wap.cnyiot.com/(S(n2nuzsitdww4rxncbfb3v4wn))/nat/pay.aspx?code=0617Nc000ZNhnP1wnt100KzBp137Nc0Y&state=18100647504"
 pub struct Electricity {
 
 }
---
+++ src/crawlers/water_fee.rs
@@ -0,0 +1,2 @@
+
+const url = "http://www.wap.cnyiot.com/(S(romfjs5urowttmo3rlzalwct))/nat/pay.aspx?code=061RxX0w3BGm203l622w3MvPmI0RxX0y&state=50000060785"
